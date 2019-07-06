e44f39c34a45667647ed86d6ed1bf846abf19fb0
philopateer <philopateer.elmaseh98@eng-st.cu.edu.eg>
2019-07-07 00:59:21 +0200

    adding bootstrap and jquery

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var chalk = require('chalk');
 var debug = require('debug')('app');
-var morgan = require('morgan')
+var morgan = require('morgan');
+var path = require('path'); 
 
 var app = express();
 app.use(morgan('tiny'));
+app.use('/css',express.static(path.join(__dirname,'/node_modules/bootstrap/dist/css')));
+app.use('/js',express.static(path.join(__dirname,'/node_modules/bootstrap/dist/js')));
+app.use('/js',express.static(path.join(__dirname,'/node_modules/jquery/dist')));
+
+
 
 app.get('/',function(req,res){
-    res.sendFile('hello');
+    res.sendFile(path.join(__dirname , 'views/index.html'));
     debug('new visitor');
 });
 
