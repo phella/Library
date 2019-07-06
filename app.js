b300d9e338a8843cfb8ccddd8ce95ffe86ee28d1
philopateer <philopateer.elmaseh98@eng-st.cu.edu.eg>
2019-07-06 18:11:34 +0200

    adding debug

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var chalk = require('chalk');
-
+var debug = require('debug')('app');
 var app = express();
 
 app.get('/',function(req,res){
@@ -8,5 +8,6 @@ app.get('/',function(req,res){
 });
 
 app.listen(3000,()=>{
-    console.log('Listenning on port '+chalk.green('3000'));
+    // Prints in debug mode only
+    debug(`Listenning on port ${chalk.green('3000')}`);
 });
