02ff4528b1d5b2ad5ce94c58a13f2aae001e1c68
philopateer <philopateer.elmaseh98@eng-st.cu.edu.eg>
2019-07-08 23:43:26 +0200

    adding eslint

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-var express = require('express');
-var chalk = require('chalk');
-var debug = require('debug')('app');
-var morgan = require('morgan');
-var path = require('path'); 
+/* eslint-disable linebreak-style */
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
-var app = express();
+const app = express();
 app.use(morgan('tiny'));
-app.use('/css',express.static(path.join(__dirname,'/node_modules/bootstrap/dist/css')));
-app.use('/js',express.static(path.join(__dirname,'/node_modules/bootstrap/dist/js')));
-app.use('/js',express.static(path.join(__dirname,'/node_modules/jquery/dist')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 
-
-
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname , 'views/index.html'));
-    debug('new visitor');
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+  debug('new visitor');
 });
 
-app.listen(3000,()=>{
-    // Prints in debug mode only
-    debug(`Listenning on port ${chalk.green('3000')}`);
+app.listen(3000, () => {
+  // Prints in debug mode only
+  debug(`Listenning on port ${chalk.green('3000')}`);
 });
