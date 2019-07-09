b75bca054058a442e7cf5f1186e6742a1c40728b
philopateer <philopateer.elmaseh98@eng-st.cu.edu.eg>
2019-07-09 13:50:27 +0200

    adding ejs view engine

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.use(morgan('tiny'));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/index.html'));
