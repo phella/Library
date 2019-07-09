2c6ddca17f8e16e874a79e678f9dbbaadbe474da
philopateer <philopateer.elmaseh98@eng-st.cu.edu.eg>
2019-07-09 15:07:37 +0200

    adding template

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
@@ -15,7 +16,7 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/index.html'));
+  res.render('index', { title: 'Library' });
   debug('new visitor');
 });
 
