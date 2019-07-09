b66c8242ae06f840c42df316663268038fb05d3a
philopateer <philopateer.elmaseh98@eng-st.cu.edu.eg>
2019-07-09 16:54:18 +0200

    add book router

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const bookRouter = express.Router();
+
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
@@ -15,8 +17,24 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist'))
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+bookRouter.route('/books')
+  .get((req, res) => {
+    res.send('Many books');
+  });
+
+bookRouter.route('/single')
+  .get((req, res) => {
+    res.send('One books');
+  });
+
+app.use('/books', bookRouter);
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Library' });
+  res.render('index', {
+    title: 'Library',
+    nav:
+      [{ link: '/books', title: 'Books' },
+        { link: '/authors', title: 'Authors' }]
+  });
   debug('new visitor');
 });
 
